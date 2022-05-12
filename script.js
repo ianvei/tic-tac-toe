68b1573ee5d13c85e5827ceab63524e6c4dbaeee
ianvei <iamianveilleux@gmail.com>
2022-05-12 11:26:51 -0400

    working on win logic

--- script.js
+++ script.js
@@ -59,9 +59,33 @@ const gameController = (function(){
 
     const player1 = Player(1);
     const player2 = Player(2);
+    let xArray = []
+    let oArray = []
 
-    const checkWin = function(){
-        console.log('are ya winning son')
+    let winningCombos = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+
+    const checkWin = function(currentValue, position){
+        
+        if (currentValue === 'X'){
+            xArray.push(position)
+        }
+        else
+            if (currentValue === 'O'){
+                oArray.push(position)
+            }
+       
+
+        for (let set of winningCombos){
+            console.log(set)
+            for (let playerPosition of xArray){
+                if (playerPosition === set[0] && playerPosition === set[1] && playerPosition === set[2]){
+                    console.log('you win!')
+                    // maybe create new array if its in the set and keep going until theres a match
+                }
+            }
+        }
+        console.log(`X array: ${xArray}`)
+        console.log(`O array: ${oArray}`)
     }
 
     const turnListener = function(){
@@ -72,16 +96,21 @@ const gameController = (function(){
                     if (object.currentValue === ''){
                         player2.chooseSpot(object)
                         turnCounter++
+                        console.log(object)
+                        gameController.checkWin(object.currentValue, object.position)
                     }
                 }
                 else {
                     if (object.currentValue === ''){
                         player1.chooseSpot(object)
                         turnCounter++
+                        console.log(object)
+                        gameController.checkWin(object.currentValue, object.position)
                     }
                     
                 }
-                gameController.checkWin()
+                
+                
             })
         }
     }
